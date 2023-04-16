a8c229cb3da6b227c2549ee442f60e60fc633eee
Caige Kelly <93157994+caige-kelly@users.noreply.github.com>
2023-04-16 01:06:14 +0000

    create root dir with openat

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+#define _POSIX_C_SOURCE 200809L
+#define _ATFILE_SOURCE
+
+#include <errno.h>
 #include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -5,15 +9,22 @@
 
 int main()
 {
+  int atfd ,dirfd, fd;
+
   /* get project name from user */
   printf("Name of project: ");
 
   char project_name[20];
   scanf("%20s", project_name);
 
-  int root_dir = mkdir(project_name, 0770);
-
+  dirfd = openat(AT_FDCWD, ".", O_DIRECTORY | O_CLOEXEC);
+  printf("%d", dirfd);
+  printf("%s", errno);
+  
+  int root_dir = mkdirat(dirfd,project_name, 0770);
+  
+  printf("%d", root_dir);
   return 0;  
 }
 
-  
+ 
